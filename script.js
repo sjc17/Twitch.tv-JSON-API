307b2e3f54afaab0f57f24465a5d3ea890b1c8e9
Steven Choi <sjc@ualberta.ca>
2018-03-17 00:46:00 -0600

    Streamer block creation implemented
    Creates blocks for each stream and shows whether they are streaming or not with links to stream + descriptions.

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 $(document).ready(function() {
   var streamerList = ["freecodecamp", "bobross", "maximilian_dood", "ESL_SC2"];
-  for (i=0; i<streamerList.length; i++) {
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+streamerList[i]+'?callback=?', function(data) { 
-      console.log(data);
-      createStreamerBlock(data);
-    });
+  var online = false;
+  for (i=0; i<streamerList.length; i++) { 
+    getChannelStatus(streamerList[i]);
   }
 });
 
-function createStreamerBlock(data) {
-  var html = '<div class="streamer container">';
-  html += '<img src="' + data.logo + '" ';
-  html += 'alt="' + data.name + '" class="inline"\/>';
-  html += '<a href="' + data.url + '" class="channel-name inline" target="_blank">';
-  html += '<h4>' + data.display_name + '<\/h4>';
-  html += '<\/a>';
-  html += '<div class="channel-info container-fluid inline">' + data.status + '<\/div>';
-  html += '<\/div>';
-  console.log(html);
-  $(".overall").append( html );
+function getChannelStatus(channelName) {
+  var onlineStatus = true;
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+channelName+'?callback=?', function(data) {
+    if (data.stream === null) {
+      onlineStatus = false;
+    }
+    createChannelBlock(data._links.channel.substr(38), onlineStatus);
+  });
 }
 
-function createOfflineBlock(data) {
-  var html = '<div class="streamer container">';
-  html += '<img src="' + data.logo + '" ';
-  html += 'alt="' + data.name + '" class="inline"\/>';
-  html += '<a href="' + data.url + '" class="channel-name inline" target="_blank">';
-  html += '<h4>' + data.display_name + '<\/h4>';
-  html += '<\/a>';
-  html += '<div class="channel-info container-fluid inline">' + data.status + '<\/div>';
-  html += '<\/div>';
-  console.log(html);
-  $(".overall").append( html );
+function createChannelBlock(channelName, onlineStatus) {
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+channelName+'?callback=?', function(data) {
+    var html = '<div class="streamer container">';
+    html += '<img src="' + data.logo + '" ';
+    html += 'alt="' + data.name + '" class="inline"\/>';
+    html += '<a href="' + data.url + '" class="channel-name inline" target="_blank">';
+    html += '<h4>' + data.display_name + '<\/h4>';
+    html += '<\/a>';
+    if (onlineStatus === true) {
+      html += '<div class="channel-info container-fluid inline">' + data.status + '<\/div>';
+    } else {
+      html += '<div class="channel-info container-fluid inline"><em>Offline<\/em><\/div>';
+    }
+    html += '<\/div>';
+    console.log(html);
+    $(".overall").append(html);
+  });
 }
